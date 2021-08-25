25214fb393a06351536ec970582d8fc06843605d
Sam Niyazi <24701208+SamNiyazi2@users.noreply.github.com>
2021-08-25 14:58:53 -0500

    Put fix for tests.  Revised root dir for deployment. Revise NPM scripts.

--- src/app/user/auth.guard.spec.ts
+++ src/app/user/auth.guard.spec.ts
@@ -1,15 +1,34 @@
+
+
+// 08/25/2021 02:48 pm - SSN
+// https://stackoverflow.com/questions/41019109/error-no-provider-for-router-while-writing-karma-jasmine-unit-test-cases
+
+import { RouterTestingModule } from '@angular/router/testing';
+
 import { TestBed, async, inject } from '@angular/core/testing';
 
 import { AuthGuard } from './auth.guard';
 
-describe('AuthGuard', () => {
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      providers: [AuthGuard]
-    });
-  });
+describe( 'AuthGuard', () => {
 
-  it('should ...', inject([AuthGuard], (guard: AuthGuard) => {
-    expect(guard).toBeTruthy();
-  }));
-});
+  // 08/25/2021 02:48 pm - SSN
+  // beforeEach( () => {
+  //   TestBed.configureTestingModule( {
+  //     providers: [ AuthGuard ]
+  //   } );
+  // } );
+  beforeEach( async( () => {
+    TestBed.configureTestingModule( {
+      providers: [ AuthGuard ],
+      imports: [ RouterTestingModule ]
+    } )
+      .compileComponents();
+  } ) );
+
+
+
+
+  it( 'should ...', inject( [ AuthGuard ], ( guard: AuthGuard ) => {
+    expect( guard ).toBeTruthy();
+  } ) );
+} );
